6c942090a825413c3eebfc48d4add7ccd14be2f6
BlackHole1410 <nathan.bigot86@gmail.com>
2025-01-09 11:27:24 +0100

    Correction Querry function

--- rag/chat/chroma.py
+++ rag/chat/chroma.py
@@ -1,28 +1,29 @@
 import chromadb
 import uuid
-from indexing.embedding import embeddings
-from indexing.contrat_text import doc
+from rag.indexing.embedding import embeddings
+from rag.indexing.contrat_text import doc
 
-chroma_client = chromadb.Client()
 
-# switch `create_collection` to `get_or_create_collection` to avoid creating a new collection every time
-collection = chroma_client.create_collection(
-name="my_collection", 
-embedding_function=embeddings)
+def Querry(query):
+    
+    chroma_client = chromadb.Client()
 
-# List of documents
-documents = doc
+    # switch `create_collection` to `get_or_create_collection` to avoid creating a new collection every time
+    collection = chroma_client.get_or_create_collection(
+    name="my_collection", 
+    embedding_function=embeddings(doc))
 
-# Generate unique IDs for the documents based on the length of the documents list
-document_ids = [str(uuid.uuid4()) for _ in range(len(documents))]
+    # List of documents
+    documents = doc
 
-# switch `add` to `upsert` to avoid adding the same documents every time
-collection.upsert(
-        documents=documents,
+    # Generate unique IDs for the documents based on the length of the documents list
+    document_ids = [str(uuid.uuid4()) for _ in range(len(documents))]
+
+    # switch `add` to `upsert` to avoid adding the same documents every time
+    collection.upsert(
+        documents=doc,
         ds=document_ids
     )
-
-def chroma(query):
     results = collection.query(
         query_texts=query,
         n_results=1 # how many results to return
